c5553506e5eaab44b39c7c375b71613847e335ac
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2023-01-30 23:54:44 +0100

    Register without otp and jwt

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,13 +5,39 @@ import { CreateUserDto } from '../dtos/create-user.dto';
 import HttpException from '../utils/exception';
 import jwt from "jsonwebtoken"
 import { JWT_EXPIRES_IN, JWT_SECRET_KEY, SECRET_KEY } from '../config';
+import UserService from './user.service';
 
 export default class AuthService {
 
     private ormService = db.getClient();
+    private userService =  new UserService()
 
-    public async login(createUserDto: CreateUserDto) {
+    public async register(createUserDto: CreateUserDto) {
+        const userByEmail = await this.userService.findByEmail(createUserDto.email)
 
+        if (userByEmail) {
+            throw new HttpException(
+                StatusCodes.CONFLICT,
+                'Email already in use'
+            )
+        }
+
+        const userByName = await this.userService.findByUsername(createUserDto.user_name)
+
+        if (userByName) {
+            throw new HttpException(
+                StatusCodes.CONFLICT,
+                'Email already in use'
+            )
+        }    
+        
+        const user = this.ormService.user.create({
+            data: {
+                user_name: createUserDto.user_name,
+                email: createUserDto.email,
+                password: await this.hashPassword(createUserDto.password)
+            }
+        }) 
     }
 
     public signJwt ( userId: string | object | Buffer  ) {
