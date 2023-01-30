e68b2ad0ec658ff47021fd819c963f21c53f3f2e
Onyeka Ikedinobi <ikedinobicruz7@gmail.com>
2023-01-30 23:28:15 +0100

    Config mod

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,9 +5,10 @@ export const CREDENTIALS = process.env.CREDENTIALS === "true";
 export const {
   NODE_ENV,
   PORT,
-  SECRET_KEY,
+  JWT_SECRET_KEY,
+  JWT_EXPIRES_IN,
   LOG_FORMAT,
   LOG_DIR,
   ORIGIN,
-  DB_URL
+  SECRET_KEY
 } = process.env;
